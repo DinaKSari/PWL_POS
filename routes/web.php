261e2137073c6d35d528c3037d987b3d8e90278e
dinaKSari <dinakumala7070@gmail.com>
2026-02-24 05:51:04 +0700

    membuat model dan menggunakan eloqient ORM untuk CRUD

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,4 @@ return view('welcome');
 });
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
