fe00a4711743f6c03a9985e44f5d5aa9db2ea2e4
dinaKSari <dinakumala7070@gmail.com>
2026-03-04 04:50:51 +0700

    CRUD menggunakan layouting

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,22 @@ Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+Route::get('/', [UserController::class, 'index']);
+Route::post('/list', [UserController::class, 'list']);
+Route::get('/create', [UserController::class, 'create']);
+Route::post('/', [UserController::class, 'store']);
+Route::get('/{id}', [UserController::class, 'show']);
+Route::get('/{id}/edit', [UserController::class, 'edit']);
+Route::put('/{id}', [UserController::class, 'update']);
+Route::delete('/{id}', [UserController::class, 'destroy']);
+// menampilkan halaman awal user
+// menampilkan data user dalam bentuk json untuk datatables
+// menampilkan halaman form tambah user
+// menyimpan data user baru
+// menampilkan detail user
+// menampilkan halaman form edit user
+// menyimpan perubahan data user
+// menghapus data user
+});
